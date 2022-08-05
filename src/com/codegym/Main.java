4b1d284a8a20df6af5fcfb84fb64608bc094b8da
Quan <quan@gmail.com>
2022-08-05 16:05:24 +0700

    done 0508

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+                System.out.println('2');
+                boolean check = true;
+                for (int i = 3; i<=100; i++){
+                    for (int j = 2 ; j < i; j++){
+                        if (i % j == 0){
+                            check = false;
+                        }
+                    }
+                    if (check){
+                        System.out.println(i);
+                    }
+                    check = true;
+                }
+
+            }
+        }
